f7293ee72522f184abbff7b6221f9411234577fc
James B <James B>
2017-04-24 20:24:42 -0500

    Made further changes to the Battle Scene, moves UI needs editing

---
+++ Yggdrasil Prototype/Assets/ShowEnemyName.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowEnemyName : MonoBehaviour {
+    private Character enemy;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        enemy = battleSystem.getInstance().Tar;
+
+        if (enemy)
+        {
+            gameObject.GetComponent<UnityEngine.UI.Text>().text = enemy.name;
+        }
+
+        else
+        {
+            gameObject.GetComponent<UnityEngine.UI.Text>().text = "FIGHT!";
+        }
+	}
+}
