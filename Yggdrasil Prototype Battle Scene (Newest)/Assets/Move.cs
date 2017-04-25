0fda36c6bd5342057438013ed750976e0f47e255
James B <James B>
2017-04-24 20:23:49 -0500

    Made further changes to the Battle Scene, moves UI needs editing

--- Yggdrasil Prototype Battle Scene (Newest)/Assets/Move.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Move {
-    protected string name;
-
-    public abstract void perform(Character character);
-}
